c7842fbd8185f97222099fe40c05de098392e6b8
Geraldine <162245702+obaobgeraldine@users.noreply.github.com>
2024-03-21 14:11:45 +0800

    Create name.c

---
+++ name.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main ()
+{
+printf(" +-------------------------------+\n");
+printf(" | hello World                   |/n");
+printf(" | My name is Geraldine G. Obaob |/n");
+printf(" | i like to play volleyball     |/n");
+printf(" +-------------------------------+/n");
+return 0
+;}
+
